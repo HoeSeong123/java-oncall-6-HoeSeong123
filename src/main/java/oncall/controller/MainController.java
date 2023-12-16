7337c2e641c947fb67ee26b21f7a678229d62e38
HoeSeong123 <ghltjd3@gmail.com>
2023-12-16 14:47:46 +0900

    feat : 비상 근무 인원 입력 과정에서 예외가 발생하면 평일 비상 근무 인원부터 재입력받는다

--- src/main/java/oncall/controller/MainController.java
+++ src/main/java/oncall/controller/MainController.java
@@ -2,13 +2,27 @@ package oncall.controller;
 
 import static oncall.util.RetryUtil.read;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import oncall.view.InputView;
 
 public class MainController {
     public void run() {
         List<String> monthAndDay = read(InputView::readMonthAndStartDay);
-        List<String> weekdayEmployee = read(InputView::readWeekdayEmployee);
-        List<String> weekEndEmployee = read(InputView::readWeekendEmployee, weekdayEmployee);
+        Map<String, List<String>> employee = read(this::readEmployee);
     }
+
+    private Map<String, List<String>> readEmployee() {
+        Map<String, List<String>> employee = new HashMap<>();
+
+        List<String> weekdayEmployee = read(InputView::readWeekdayEmployee);
+        List<String> weekEndEmployee = InputView.readWeekendEmployee(weekdayEmployee);
+
+        employee.put("평일", weekdayEmployee);
+        employee.put("휴일", weekEndEmployee);
+
+        return employee;
+    }
+
 }
