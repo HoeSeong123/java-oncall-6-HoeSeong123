3cbb620d67695c1531797b872366d0b6ac18712e
HoeSeong123 <ghltjd3@gmail.com>
2023-12-16 14:20:27 +0900

    feat : 평일 비상 근무 인원을 입력한다

--- src/main/java/oncall/controller/MainController.java
+++ src/main/java/oncall/controller/MainController.java
@@ -2,10 +2,12 @@ package oncall.controller;
 
 import static oncall.util.RetryUtil.read;
 
+import java.util.List;
 import oncall.view.InputView;
 
 public class MainController {
     public void run() {
-        read(InputView::readMonthAndStartDay);
+        List<String> monthAndDay = read(InputView::readMonthAndStartDay);
+        List<String> weekdayEmployee = read(InputView::readWeekdayEmployee);
     }
 }
--- src/main/java/oncall/view/InputView.java
+++ src/main/java/oncall/view/InputView.java
@@ -19,8 +19,17 @@ public class InputView {
         return Util.splitToList(COMMA.get(), input);
     }
 
+    public static List<String> readWeekdayEmployee() {
+        System.out.print(Message.INPUT_WEEKDAY_EMPLOYEE.message);
+        String input = Console.readLine();
+
+        return Util.splitToList(COMMA.get(), input);
+    }
+
     private enum Message {
-        INPUT_MONTH_START_DAY("비상 근무를 배정할 월과 시작 요일을 입력하세요> ");
+        INPUT_MONTH_START_DAY("비상 근무를 배정할 월과 시작 요일을 입력하세요> "),
+        INPUT_WEEKDAY_EMPLOYEE("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+
 
         private final String message;
 
